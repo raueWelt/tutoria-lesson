431a3df82557fde70cb4df047f8eb59018407e04
raueWelt <wresni.business@gmail.com>
2022-10-22 23:14:23 +0700

    Initialize project

---
+++ lesson-app/src/main/java/rw/tutoria/lesson/app/LessonAppApplication.java
@@ -0,0 +1,13 @@
+package rw.tutoria.lesson.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LessonAppApplication.class, args);
+    }
+
+}
